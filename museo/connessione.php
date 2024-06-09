a4cde02dc40171d3914e8a25c36fde377a4ce256
Devo21 <mboulefacklekaneloic@gmail.com>
2024-06-09 11:08:24 +0100

    change database credentials

--- museo/connessione.php
+++ museo/connessione.php
@@ -1,8 +1,8 @@
 <?php
 
     $SERVER = "localhost";          // oppure "127.0.0.1"
-    $USER = "virgil";
-    $PASSWORD = "studente";
+    $USER = "root";			// virgil
+    $PASSWORD = "";			// studente
     $DB_NAME = "Museo";
 	$link = mysqli_connect($SERVER, $USER, $PASSWORD, $DB_NAME);
 
